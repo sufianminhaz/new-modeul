522c28a903e84a26386e48adefc7c365b4bf5c08
sufianminhaz <sufian072024@gmail.com>
2025-03-06 21:29:38 +0600

    know about constructor

---
+++ java/ecma.js
@@ -0,0 +1,19 @@
+// const friends =['rahim', 'karim', 'bangladesh']
+
+// const [first , second] = friends;
+
+// const newFriends =['hero alom',...friends]
+// console.log(newFriends)
+
+// const numbers = [1,2,3,4,56,8]
+
+// const newArray = numbers.map(value => value+1)
+// console.log(newArray)
+
+
+const numbers = [3, 10, 7, 6, 2, 8, 15, 4];
+
+const newNumber = numbers.map(value => value % 2 == 0)
+
+console.log(newNumber)
+
